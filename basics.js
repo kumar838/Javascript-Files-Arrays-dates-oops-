1db2d7887d953a06f28a9ecd84f30f72401247ef
kumar838 <31312812+kumar838@users.noreply.github.com>
2019-02-01 19:23:30 +0530

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,15 @@
 
+typeof "John"              // Returns "string" 
+typeof 3.14                // Returns "number"
+typeof true                // Returns "boolean"
+typeof false               // Returns "boolean"
+typeof x                   // Returns "undefined" (if x has no value)
+
+
+typeof {name:'John', age:34} // Returns "object"
+typeof [1,2,3,4]             // Returns "object" (not "array", see note below)
+typeof null                  // Returns "object"
+typeof function myFunc(){}   // Returns "function"
+
+console.log(null==undefined);  //  true
+console.log(null===undefined); // false
